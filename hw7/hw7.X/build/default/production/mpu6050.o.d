ac544dc182753fb81b4d6433e62f1fb66c21b460
bhagyeshagresar <bhagyeshagresar@gmail.com>
2022-05-06 15:43:21 -0500

    finished hw8

---
+++ hw7/hw7.X/build/default/production/mpu6050.o.d
@@ -0,0 +1,5 @@
+build/default/production/mpu6050.o: mpu6050.c i2c_master.h mpu6050.h
+
+i2c_master.h:
+
+mpu6050.h:
---
+++ hw8/hw8.X/build/default/production/main.o.d
@@ -0,0 +1,7 @@
+build/default/production/main.o: main.c i2c_master.h ssd1306.h font.h
+
+i2c_master.h:
+
+ssd1306.h:
+
+font.h:
